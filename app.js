3b66d316c7f02c6bdcd3fe86c4d419c39cba6c98
Yubhav Seth <yubhav.seth97@gmail.com>
2020-04-10 13:57:00 +0000

    EJS file updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'),
-	  bodyParser = require('body-parser'),
-	  request = require('request'),
-	  app = express();
+bodyParser = require('body-parser'),
+request = require('request'),
+app = express()
 
 const apiKey = '19ca2f980f1a2e5b6d880907194222c2';
 
@@ -17,6 +17,21 @@ app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error! You fucked up.'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error! You fucked up.'});
+      } else {
+        let weatherText = `${weather.main.temp} degrees Fahrenheit in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
 app.listen(3000,function(){
-	console.log("The server is active now");
+	console.log("Server started.");
 })
